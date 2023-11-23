d80ce42d606327e82ba3da35d47e2d1d55d76060
Sachintha Chathuranga <smschathu1234@gmail.com>
2023-11-23 23:00:19 +0530

    second commit

--- frontend/curd-list/src/Home.jsx
+++ frontend/curd-list/src/Home.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import './home.css';
 const Home = () => {
   return (
     <div className="main">
       <h1>This is Home page</h1>
-      <Link to="/students">List</Link>
+      <Link to="/students" className="btn btn-list">List</Link>
     </div>
   );
 };
